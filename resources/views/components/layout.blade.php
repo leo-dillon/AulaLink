c527e3d9bf84dd79529a522767944d227ffb0038
Leonardo Dillon <jeannotegui@gmail.com>
2025-05-15 20:14:56 -0300

    feat(styles): add and configure Tailwind CSS

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ url('css/style.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+     @vite('resources/css/app.css')
 
 
 </head>
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,6 +1,7 @@
 <x-layout>
     <x-slot:title> AUTH </x-slot:title>
-<h1>ELEGUIR SI INICIAR SESION O REGUISTRARSE</h1>
+<h1 class="font-sm text-red-200" >ELEGUIR SI INICIAR SESION O REGUISTRARSE</h1>
     <a href="{{ route('login.store') }}"> LOGIN</a>
     <a href="{{ route('register.store') }}">REGISTRARSE</a>
+    <p>hola</p>
 </x-layout>
