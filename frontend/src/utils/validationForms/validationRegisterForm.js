bb8d6cfe42f64af974f877eb62df80cc7b6a0e48
Leonardo Dillon <jeannotegui@gmail.com>
2025-05-21 00:25:05 -0300

    test(Register) Generando la validación del Registro de usaurio

---
+++ frontend/src/utils/validationForms/validationRegisterForm.js
@@ -0,0 +1,45 @@
+import { validateAddress, validateBirthdate, validateDNI, validateEmail, validateFirstName, validateGenre, validateLastName, validatePassword, validatephone } from "../validations.js"
+
+export function validateRegisterForm( data ){
+    const errors = {}
+    const { 
+        firstName, 
+        lastName,
+        gender,
+        birthdate,
+        dni, 
+        email, 
+        address,
+        phone,
+        password
+    } = data
+
+    const resultValidationFistName = validateFirstName(firstName)
+    if( resultValidationFistName.length > 3 ) errors.firstName = resultValidationFistName 
+
+    const resultValidationLastName = validateLastName(lastName)
+    if( resultValidationLastName.length > 3 ) errors.lastName = resultValidationLastName
+    
+    const resultValidationGenre = validateGenre(gender)
+    if( resultValidationGenre.length > 3 ) errors.gender = resultValidationFistName
+
+    const resultValidationBirthDate = validateBirthdate(birthdate) 
+    if( resultValidationBirthDate.length > 3 ) errors.birthdate = resultValidationBirthDate
+
+    const resultValidationDNI = validateDNI(dni)
+    if( resultValidationDNI.length > 3 ) errors.dni = resultValidationDNI
+
+    const resultValidationGmail = validateEmail(email)
+    if( resultValidationGmail.length > 3 ) errors.email = resultValidationGmail
+
+    const resultValidationAddress = validateAddress(address)
+    if( resultValidationAddress ) errors.address = resultValidationAddress
+
+    const resultValidationphone = validatephone(phone)
+    if( resultValidationphone ) errors.phone = resultValidationphone
+
+    const resultValidationPassword = validatePassword(password)
+    if( resultValidationPassword ) errors.password = resultValidationPassword
+
+    return errors
+}
---
+++ frontend/src/utils/validations.js
@@ -0,0 +1,96 @@
+
+export function validateFirstName(firstName){
+    let message = ""
+    if(!firstName) message += "Debes completar este campo."
+    if(firstName.trim().length < 3) message += "El nombre debe tener más de 3 caracteres."
+    return message
+}
+
+export function validateLastName(lastName){
+    let message = ""
+    if(!lastName) message += "Debes completar este campo"
+    if(lastName.length < 3) message += "El apellido debe tener más de 3 caracteres."
+    return message
+}
+
+export function validateGenre(genre){
+    let message = ""
+    if(!genre) message += "Debes completar este campo"
+    return message
+}
+
+export function validateBirthdate(birthdate){
+    console.log(birthdate)
+    let message = ""
+    const date = new Date(birthdate)
+    const today = new Date()
+    const ageDif = today.getFullYear() - date.getFullYear() // Esto calcula la edad en años
+    const monthDif = today.getMonth() - date.getMonth()     // Se usará para saber si cumplio o no años
+    const dayDif = today.getDate() - date.getDate()         // Se usará para saber si cumplio o no años
+    
+    const age = ageDif
+    if( monthDif < 0 || ( monthDif == 0 && dayDif < 0 ) ){  // si mothDif menor a 0 aun no cumplio años
+        age -= 1                                            // Si mothDif es 0 y dayDif es menor a 0 aun no cumplio años
+    }                                                       // Contrarío. Si cumplio años
+
+    if (!birthdate)  message += "Debes completar este campo"
+    if (age < 13)   message += 'Debes tener al menos 13 años.'
+    if (age > 100)  message += 'La edad no puede ser mayor a 100 años.'
+    
+    return message
+}
+
+export function validateDNI(dni){
+    let message = ""
+    let cleanDNI = dni.toString().trim()
+
+    if(!cleanDNI) message += "Debes completar este campo"
+    if(!/^\d+$/.test(cleanDNI) ) message += "El DNI no puede contener letras"
+    if( cleanDNI.length != 8 ) message += "El DNI debe tener 8 caracteres"
+
+    return message
+}
+
+export function validateEmail(email){
+    let message = ""
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if(!email) message += "Debes incluir este campo"
+    if( !regex.test(email) ) message += "Email invalido"
+
+    return message
+}
+
+export function validateAddress(address){
+    let message = ""
+
+    if(!address) message += "Debes incluir este campo"
+    if(address.length < 4) message += "La dirección debe ser más especifica"
+
+    return message
+}
+
+export function validatephone( phone ){
+    let message = ""
+    if (!phone) message += 'Debes ingresar un número de teléfono.'
+
+    const cleaned = phone.toString().replace(/\D/g, '') // quitar espacios y símbolos
+
+    if (!/^\d+$/.test(cleaned)) {
+        message += 'El teléfono solo puede contener números.'
+    }
+
+    if (cleaned.length !== 10) {
+        message += 'El número debe tener exactamente 10 dígitos (sin 0 ni 15).'
+    } // ejemplo 2324 697491 => interior BSAR 10 caracteres sin espacios
+
+    return message
+}
+
+export function validatePassword(password){
+    let message = ""
+    
+    if( !password ) message += "Debes completar este campo"
+    if( password.length < 3 ) message += "La contraseña debe tener más de 3 caracteres" 
+    
+    return message
+}
