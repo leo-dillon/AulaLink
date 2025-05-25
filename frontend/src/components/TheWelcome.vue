69cb56f59934415767ff0a811b9d028fcac0de4e
Ticiano Piccino <piccinoticiano@gmail.com>
2025-05-25 12:51:16 -0300

    enpezando la OnePage

--- frontend/src/components/TheWelcome.vue
+++ frontend/src/components/TheWelcome.vue
@@ -1,5 +1,85 @@
 <template>
-  <main class="min-h-screen flex items-center justify-center">
-    <p class="text-5xl"> Este es un inicio muy vistoso y hermoso </p>
-  </main>
+  <!-- Hero -->
+   <div class="bg-[#EBE2D3]">
+  <section class="max-w-6xl  mx-auto flex flex-col md:flex-row items-center justify-between px-6 pt-10 text-center md:text-left">
+  <!-- Texto a la izquierda -->
+  <div class="md:w-1/2 text-center flex flex-col items-center">
+  <h1 class="text-2xl md:text-5xl font-bold mb-4">
+    Organiza tu comunidad educativa
+  </h1>
+  <p class="mb-6 text-2xl max-w-xl">
+    Te ayudamos a organizar y simplificar la vida escolar de todas las personas que forman parte de tu comunidad educativa.
+  </p>
+  <button class="bg-blue-900 text-white px-6 py-2 rounded shadow hover:bg-blue-800">
+    Contactar a Ventas
+  </button>
+</div>
+
+  <!-- Imagen a la derecha -->
+  <div class="md:w-1/2 mt-8 md:mt-0 flex justify-center">
+    <img src="/img/img_1_OnePage.png" alt="Imagen OnePage 1" class="w-full max-w-md">
+  </div>
+</section>
+  <!-- ¿Qué es Aulalink? -->
+  <section class="px-6  flex flex-col md:flex-row items-center gap-8 max-w-6xl mx-auto">
+    <div class="md:w-1/2">
+      <img src="/img/img_2_OnePage.png" alt="¿Qué es AulaLink?" class="w-full max-w-sm mx-auto">
+    </div>
+    <div class="md:w-1/2 text-center md:text-left">
+      <h2 class="text-3xl font-bold mb-4">¿Qué es AulaLink?</h2>
+      <p class="text-gray-700 text-lg">
+        AulaLink es una plataforma pensada para instituciones educativas que desean optimizar su comunicación, organización de tareas, gestión de estudiantes, docentes y familias en un solo lugar, facilitando el trabajo diario y fortaleciendo el vínculo con toda la comunidad escolar.
+      </p>
+    </div>
+  </section>
+
+  <!-- Beneficios -->
+  <section class="px-6   md:flex-row items-center gap-8 max-w-3xl mx-auto">
+  <h2 class="text-3xl font-bold mb-8">Beneficios de usar AulaLink</h2>
+
+  <div class="grid ">
+    <div class="flex gap-4 items-start">
+      <div class="max-w-3/4">
+        <h3 class="font-bold text-2xl">Organización Centralizada</h3>
+        <p class=" text-lg">Gestioná tareas, horarios, comunicaciones y archivos desde un único lugar, Mejorando la eficiencia y reduciendo el tiempo en gestiones administrativas.</p>
+      </div>
+      <img src="/img/icono_1.png" class="max-w-1/4" alt="Centralización">
+    </div>
+
+    <div class="flex gap-4 items-start">
+      <div class="max-w-3/4">
+        <h3 class="font-bold text-2xl">Gestión de Tareas</h3>
+        <p class=" text-lg">Asigná, realizá y hacé seguimiento de tareas fácilmente, facilitando la planificación docente y garantizando que estudiantes y familias estén siempre informados y comprometidos.</p>
+      </div>
+      <img src="/img/icono_2.png" class="max-w-1/4" alt="Gestión de tareas">
+    </div>
+
+    <div class="flex gap-4 items-start">
+      <div class="max-w-3/4">
+        <h3 class="font-bold text-2xl">Calendario Integrado</h3>
+        <p class=" text-lg">Tené a mano fechas importantes, reuniones, entregas y más, con recordatorios automáticos y una vista clara del calendario para que toda la comunidad educativa esté siempre informada y coordinada.</p>
+      </div>
+      <img src="/img/icono_3.png" class="max-w-1/4" alt="Calendario Integrado">
+    </div>
+  </div>
+</section>
+
+
+  <!-- Marcar la diferencia -->
+  <section class="px-6 py-12 text-center">
+    <div class=" mx-auto ">
+      <h2 class="text-3xl font-bold mb-4">Marcá la diferencia</h2>
+      <p class="  text-lg ">Escuelas más conectadas, organizadas y enfocadas en educar. </p>
+    <p class="mb-8  text-lg "> gracias a <strong>AulaLink.</strong></p>
+  </div>
+    
+    <div class="flex justify-center items-center gap-6 flex-wrap">
+     <img src="/img/Institucion5.png" alt=" LogoEscuela 2" class="max-h-20">
+      <img src="/img/Institucion3.png" alt=" LogoEscuela 2" class="max-h-20">
+      <img src="/img/Institucion1.png" alt=" LogoEscuela 3" class="max-h-40">
+      <img src="/img/Institucion2.png" alt=" LogoEscuela 4" class="max-h-20">
+      <img src="/img/Institucion4.png" alt=" LogoEscuela 2" class="max-h-20">
+    </div>
+  </section>
+  </div>
 </template>
